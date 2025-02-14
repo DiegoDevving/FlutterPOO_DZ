9e2cf47c0499f42afa449200d86ac7b818ce20dd
DiegoDev <diego.zavaleta4002@gmail.com>
2025-02-14 11:30:45 -0600

    Starting TabBarView

---
+++ donut_app_2b_zavaleta/lib/Pages/home_page.dart
@@ -0,0 +1,87 @@
+import 'package:donut_app_2b_rodriguez/tabs/burger_tab.dart';
+import 'package:donut_app_2b_rodriguez/tabs/donut_tab.dart';
+import 'package:donut_app_2b_rodriguez/tabs/pancakes_tab.dart';
+import 'package:donut_app_2b_rodriguez/tabs/pizza_tab.dart';
+import 'package:donut_app_2b_rodriguez/tabs/smoothie_tab.dart';
+import 'package:donut_app_2b_rodriguez/utils/my_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Lista de tabs
+  List<Widget> myTabs = [
+    MyTab(iconPath: 'lib/icons/donut.png'),
+    MyTab(iconPath: 'lib/icons/burger.png'),
+    MyTab(iconPath: 'lib/icons/smoothie.png'),
+    MyTab(iconPath: 'lib/icons/pancakes.png'),
+    MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            // Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text('I want to ', style: TextStyle(fontSize: 32)),
+                  Text('Eat',
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline))
+                ],
+              ),
+            ),
+            // TabBar
+            TabBar(tabs: myTabs),
+            // TabBarView with content for each tab
+            Expanded(
+              child: TabBarView(
+                children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothieTab(),
+                  PancakesTab(),
+                  PizzaTab(),
+                ],
+              ),
+            ),
+            // Carrito (aquí falta el código para el carrito si lo necesitas)
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Function to create content for each tab
+  Widget _buildTabContent(String foodType) {
+    return Center(
+      child: Text(
+        'Delicious $foodType coming soon!',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
---
+++ donut_app_2b_zavaleta/lib/tabs/donut_tab.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DonutTab extends StatelessWidget {
+  //Lista de donas
+  final List donutsOnsale = [
+    //[onutFlavor, donutPrice, donutColor, imageName]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/Strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "395", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
+   DonutTab({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      //Elementos de nuestra lista
+      itemCount: donutsOnsale.length,
+      padding: EdgeInsets.all(8.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //Numero de columnas
+        crossAxisCount: 2),
+        itemBuilder: (context, index){
+          return DonutTile(
+            donutFlavor: donutsOnsale[index][0],
+            donutPrice: donutsOnsale[index][1],
+            DonutColor: donutsOnsale[index][2],
+            imageName: donutsOnsale[index][3]
+          );
+        },
+    );
+  }
+}
